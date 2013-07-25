81a4ff59b696dd1c092fbec75ab11d3f845cbd02
Andy M <andy.dude@gmail.com>
2013-07-25 08:10:13 -0500

    Added some async stuff to simulate webservices

--- TrainingWeb/Scripts/TrainingReview.html.js
+++ TrainingWeb/Scripts/TrainingReview.html.js
@@ -4,16 +4,21 @@
 /// <reference path="Libraries/angular.min.js" />
 angular.module('trainingReview.service', []).
   value('training', {
-      retrieve: function (id) {
-          return new Training("The Trusted Advisor", 1);
+      retrieve: function (id, callback) {
+          window.setTimeout(function () {
+              callback(new Training("The Trusted Advisor", 1));
+          }, 1000);
       }
   }).
   value('employeetraining', {
-      retrieve: function (trainingId) {
-          return [new EmployeeTraining("Vickie - 5/5/2013", 1, "This was the greatest book ever!", 1, "Vickie S.", new Date(2013, 5, 5), 5),
-              new EmployeeTraining("Shari - 4/4/2013", 2, "I could have written this book", 2, "Shari S.", new Date(2013, 4, 4), 1),
-              new EmployeeTraining("Dave - 1/1/2013", 3, "All I can say was Meh", 3, "Dave H.", new Date(2013, 5, 5), 3),
-              new EmployeeTraining("Hodor - 5/5/2013", 4, "Hodor! Hodor! Hodor!", 4, "Hodor H.", new Date(2013, 6, 6), 4)];
+      retrieve: function (trainingId, callback) {
+          window.setTimeout(function(){
+              callback([new EmployeeTraining("Vickie - 5/5/2013", 1, "This was the greatest book ever!", 1, "Vickie S.", new Date(2013, 5, 5), 5),
+                  new EmployeeTraining("Shari - 4/4/2013", 2, "I could have written this book", 2, "Shari S.", new Date(2013, 4, 4), 1),
+                  new EmployeeTraining("Dave - 1/1/2013", 3, "All I can say was Meh", 3, "Dave H.", new Date(2013, 5, 5), 3),
+                  new EmployeeTraining("Hodor - 5/5/2013", 4, "Hodor! Hodor! Hodor!", 4, "Hodor H.", new Date(2013, 6, 6), 4)]);
+          },
+          1000);
       }
   }).
   value('user', {
@@ -32,12 +37,12 @@ angular.module('trainingReview.directive', []).directive('starRating', function 
             rating: '=rating',
             isEditable: '=isEditable'
         },
-        template: ['<div>',
-            '<img ng-src="{{rating > 0 ? \'/images/star-gold32.png\'  : \'/images/star-white32.png\'}}" />',
-            '<img ng-src="{{rating > 1 ? \'/images/star-gold32.png\'  : \'/images/star-white32.png\'}}" />',
-            '<img ng-src="{{rating > 2 ? \'/images/star-gold32.png\'  : \'/images/star-white32.png\'}}" />',
-            '<img ng-src="{{rating > 3 ? \'/images/star-gold32.png\'  : \'/images/star-white32.png\'}}" />',
-            '<img ng-src="{{rating > 4 ? \'/images/star-gold32.png\'  : \'/images/star-white32.png\'}}" />'].join(''),
+        template: ['<div class="star-rating">',
+            '<img ng-src="{{rating > 0 ? \'images/stargold32.png\'  : \'images/starwhite32.png\'}}" />',
+            '<img ng-src="{{rating > 1 ? \'images/stargold32.png\'  : \'images/starwhite32.png\'}}" />',
+            '<img ng-src="{{rating > 2 ? \'images/stargold32.png\'  : \'images/starwhite32.png\'}}" />',
+            '<img ng-src="{{rating > 3 ? \'images/stargold32.png\'  : \'images/starwhite32.png\'}}" />',
+            '<img ng-src="{{rating > 4 ? \'images/stargold32.png\'  : \'images/starwhite32.png\'}}" />'].join(''),
         link: function (scope, element, attrs, transclusion) {
             // Title element
             //var title = angular.element(element.children()[0]),
@@ -50,9 +55,9 @@ angular.module('trainingReview.directive', []).directive('starRating', function 
                     var img = $(this);
                     scope.rating = img.prevAll().length + 1;
                     scope.$root.$digest();
-                    img.nextAll().attr('src', 'images/star-white32.png');
-                    img.prevAll().attr('src', 'images/star-gold32.png');
-                    img.attr('src', 'images/star-gold32.png');
+                    img.nextAll().attr('src', 'images/starwhite32.png');
+                    img.prevAll().attr('src', 'images/stargold32.png');
+                    img.attr('src', 'images/stargold32.png');
                 });
             }
         }
@@ -70,9 +75,19 @@ angular.module('trainingReview', ['trainingReview.service', 'trainingReview.dire
 
 // A Controller for your app
 var trainingController = function ($scope, training, employeetraining, user) {
-    $scope.training = training.retrieve(1);
+    var scope = $scope;
+    scope.isLoaded = false;
+    training.retrieve(1, function retrieveCallback(result) {
+        $scope.training = result;
+        employeetraining.retrieve(result.Id, function retrieveTrainingsCallback(results) {
+            $scope.training.EmployeeTrainings = results;
+            scope.isLoaded = true;
+            scope.$digest();
+
+        });
+    });
+
     $scope.user = user.retrieve();
-    $scope.training.EmployeeTrainings = employeetraining.retrieve(1);
     $scope.addReview = function () {
         var length = $scope.training.EmployeeTrainings.length;
         for (var i = 0; i < length; i++) {
@@ -95,5 +110,12 @@ var trainingController = function ($scope, training, employeetraining, user) {
         if (employeeTraining.UserId == $scope.user.Id) {
             employeeTraining.IsEditable = true;
         }
-    }
+    };
+
+    $scope.formatDate = function (date) {
+        if (!date)
+            return '';
+        else 
+            return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()
+    };
 }
